06884d1fce5d1b0fb85778fd018b2aa221c37544
Abhijeet <abhijeet4099@gmail.com>
2023-02-04 12:20:42 +0530

    Add RestTemplate to consume MovieInfo API

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/controller/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/controller/MovieCatalogController.java
@@ -1,21 +1,41 @@
 package com.example.moviecatalogservice.controller;
 
 import com.example.moviecatalogservice.model.CatalogItem;
+import com.example.moviecatalogservice.model.Movie;
+import com.example.moviecatalogservice.model.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogController {
+    @Autowired
+    private RestTemplate restTemplate;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-        return Collections.singletonList(
-                new CatalogItem("Jason Bourne", "Test", 7)
+
+
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 7),
+                new Rating("6789", 9)
         );
+
+        return  ratings.stream().map( rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(),"desc", rating.getRating());
+        }).collect(Collectors.toList());
+//        return Collections.singletonList(
+//                new CatalogItem("Jason Bourne", "Test", 7)
+//        );
     }
 }
---
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/model/Movie.java
@@ -0,0 +1,31 @@
+package com.example.moviecatalogservice.model;
+
+public class Movie {
+    public String movieId;
+
+    public Movie() {
+    }
+
+    public String name;
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Movie(String movieId, String name) {
+        this.movieId = movieId;
+        this.name = name;
+    }
+}
