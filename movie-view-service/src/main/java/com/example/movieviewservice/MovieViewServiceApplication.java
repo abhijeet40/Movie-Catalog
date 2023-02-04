2f8848d600a52d1f60ce22cfd8454a48f89ad190
Abhijeet <abhijeet4099@gmail.com>
2023-02-04 07:53:35 +0530

    Init 3 projects

---
+++ movie-view-service/src/main/java/com/example/movieviewservice/MovieViewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.movieviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieViewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieViewServiceApplication.class, args);
+	}
+
+}
---
+++ movie-view-service/src/test/java/com/example/movieviewservice/MovieViewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movieviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieViewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
